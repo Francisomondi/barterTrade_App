b484bf190d29f1fe4c5f9d3a17215ea9d69d409b
Francis Omondi <francisomondi17@gmail.com>
2026-08-30 15:46:27 +0300

    login and registration

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,10 @@
 import { useNavigate } from "react-router-dom";
-
 import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
   const navigate = useNavigate();
 
-  const {
-    user,
-    logout,
-  } = useAuth();
+  const {user,logout} = useAuth();
 
   const handleLogout = () => {
     logout();
